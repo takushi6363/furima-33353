d8eeba64cf721c2923a874c8d67dfbf3b0c69c0d
takushi_akayama <akayama63@gmail.com>
2021-03-10 15:53:48 +0900

    FactoryBotにコード記述

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {'テスト'}
+    email                 {'test@test.com'}
+    password              {'a000000'}
+    password_comfirmation {password}
+    last_name             {'鈴木'}
+    first_name            {'太郎'}
+    last_name_kana        {'スズキ'}
+    first_name_kana       {'タロウ'}
+    birthday              {'1950-01-01'}
+  end
+end
