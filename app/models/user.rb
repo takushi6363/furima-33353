fd98f93443e68596fb5cca8ac7fad09b2d156b7d
takushi_akayama <akayama63@gmail.com>
2021-03-11 18:06:12 +0900

    ルボコップを実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,30 +6,29 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :password, presence: true,
-                       format:{
+                       format: {
                          with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i,
-                         message: "６文字以上で英字と数字の両方を含めて設定して下さい"
+                         message: '６文字以上で英字と数字の両方を含めて設定して下さい'
                        }
   validates :last_name, presence: true,
-                        format:{
+                        format: {
                           with: /\A[一-龥ぁ-ん]/,
-                          message: "全角で入力して下さい"
-                        } 
+                          message: '全角で入力して下さい'
+                        }
   validates :first_name, presence: true,
-                        format:{
-                          with: /\A[一-龥ぁ-ん]/,
-                          message: "全角で入力して下さい"
-                        } 
+                         format: {
+                           with: /\A[一-龥ぁ-ん]/,
+                           message: '全角で入力して下さい'
+                         }
   validates :last_name_kana, presence: true,
-                        format:{
-                          with: /\A[ァ-ヶー－]+\z/,
-                          message:"全角カタカナで入力して下さい"
-                        }
+                             format: {
+                               with: /\A[ァ-ヶー－]+\z/,
+                               message: '全角カタカナで入力して下さい'
+                             }
   validates :first_name_kana, presence: true,
-                        format:{
-                          with: /\A[ァ-ヶー－]+\z/,
-                          message:"全角カタカナで入力して下さい"
-                        }
+                              format: {
+                                with: /\A[ァ-ヶー－]+\z/,
+                                message: '全角カタカナで入力して下さい'
+                              }
   validates :birthday, presence: true
-
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {'テスト'}
-    email                 {'test@test.com'}
-    password              {'a000000'}
-    password_confirmation {password}
-    last_name             {'鈴木'}
-    first_name            {'太郎'}
-    last_name_kana        {'スズキ'}
-    first_name_kana       {'タロウ'}
-    birthday              {'1950-01-01'}
+    nickname              { 'テスト' }
+    email                 { 'test@test.com' }
+    password              { 'a000000' }
+    password_confirmation { password }
+    last_name             { '鈴木' }
+    first_name            { '太郎' }
+    last_name_kana        { 'スズキ' }
+    first_name_kana       { 'タロウ' }
+    birthday              { '1950-01-01' }
   end
 end
