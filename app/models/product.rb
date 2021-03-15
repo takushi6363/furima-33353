e6cf25d2d0527340378565067969f1dc7c8dafc5
takushi_akayama <akayama63@gmail.com>
2021-03-15 22:07:16 +0900

    activhash カテゴリー記載

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,10 @@
 class Product < ApplicationRecord
   belongs_to :user
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :product_category
+              
+  validates :product_name, :product_explanation, presence: true
+
+  validates :Product_category_id, numericality: { other_than: 1 }
 end
