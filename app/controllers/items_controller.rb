6af23a1c897d24cf96f5f2e0782ae66820f003f3
takushi_akayama <akayama63@gmail.com>
2021-03-16 21:49:56 +0900

    商品出品　コントローラーの記載

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,21 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @product = Product.new
   end
-  
+
+  def create
+    @product = Product.create(create_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render new_item_path
+    end
+
+  end
+
+private
+def create_params
+ params.require(:prodct).permit(:product_name,:product_explanation,:product_category_id,:product_status_id,:shipping_charges_id,:delivery_area_id,:days_to_ship_id,:price).merge(user_id: current_user.id)
+
 end
