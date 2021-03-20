c6c70fd7a970016af7a44e09aa3059d306583b2d
takushi_akayama <akayama63@gmail.com>
2021-03-20 16:13:17 +0900

    メッセージエラー追記

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,19 +10,20 @@ class Product < ApplicationRecord
   belongs_to :delivery_area
   belongs_to :days_to_ship
 
-  validates :product_category_id, numericality: { other_than: 1 }
-  validates :product_status_id, numericality: { other_than: 1 }
-  validates :shipping_charges_id, numericality: { other_than: 1 }
-  validates :delivery_area_id, numericality: { other_than: 1 }
-  validates :days_to_ship_id, numericality: { other_than: 1 }
+  validates :product_category_id, numericality: { other_than: 1 ,message: '選択して下さい'}
+  validates :product_status_id, numericality: { other_than: 1 ,message: '選択して下さい'}
+  validates :shipping_charges_id, numericality: { other_than: 1 ,message: '選択して下さい'}
+  validates :delivery_area_id, numericality: { other_than: 1 ,message: '選択して下さい'}
+  validates :days_to_ship_id, numericality: { other_than: 1 ,message: '選択して下さい'}
   validate :image_presence
 
-  with_options presence: true do
+  with_options presence: { message:"文字を入力して下さい"} do
   validates :product_name
   validates :product_explanation
   validates :price,
-             format: { with: /\A[0-9]+\z/ },
-             numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+             format: { with: /\A[0-9]+\z/ ,message: '半角数字で入力して下さい'},
+             numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999,message: '300〜9,999,999の範囲で入力して下さい'}
+
   end
 
     def image_presence
