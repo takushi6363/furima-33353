ab2c9164ad1f3fcd5f08bf8129c8725047d524f5
takushi_akayama <akayama63@gmail.com>
2021-03-20 16:53:00 +0900

    ProdcutのFactoryBotを記述

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :product do
-    
+    product_name              { 'テスト' }
+    product_explanation       { 'テスト' }
+    product_category_id       { 2 }
+    product_status_id         { 2 }
+    shipping_charges_id       { 2 }
+    delivery_area_id          { 2 }
+    days_to_ship_id           { 2 }
+    price                     { 1000 }
+    association :user
+
+    after(:build) do |product|
+      product.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+
   end
 end
