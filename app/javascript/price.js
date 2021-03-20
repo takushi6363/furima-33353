42e6c04dcb7dffa303b7f82a0c382a9dd6d609f7
takushi_akayama <akayama63@gmail.com>
2021-03-20 14:19:39 +0900

    販売価格のjavasclipt記載

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,2 +1,14 @@
-  const price = document.getElementsByClassName("question")
-  console.log(price)
+function check(){
+  const itemPrice = document.getElementById("item-price")
+  itemPrice.addEventListener('input',function(){
+    let fee = itemPrice.value * 0.1
+    let profitPrice = itemPrice.value - fee
+    let addTaxPrice = document.getElementById('add-tax-price');
+    addTaxPrice.innerText =`${fee}`
+    let profit = document.getElementById('profit');
+    profit.innerText = `${profitPrice}`
+  })
+}
+
+window.addEventListener("load", check);
+
