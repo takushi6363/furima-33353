f55f432b4ad9d6ab2d4242d419d9190c5114e7c6
takushi_akayama <akayama63@gmail.com>
2021-04-04 23:31:40 +0900

    ordersコントローラーの記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,23 @@
 class OrdersController < ApplicationController
 
   def index
+    @user_buy = UserBuy.new
   end
-  
+
+  def create
+    @user_buy = UserBuy.new(buy_params)
+    if @user_buy.valid?
+      @user_buy.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+
+    private
+   def buy_params
+    params.require(:user_buy).permit(:shipping_postal_code, :delivery_area_id, :shipping_municipality, :shipping_address, :shipping_building_name, :phone_number)
+   end
+
 end
