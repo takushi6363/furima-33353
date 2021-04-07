33bf8f156eed9672fe07a4db06b7df3105de5bb9
takushi_akayama <akayama63@gmail.com>
2021-04-07 14:42:35 +0900

    orders/controllerの可読性を整える為記載変更

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,25 +1,20 @@
 class OrdersController < ApplicationController
-
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :set_product,only: [:index, :create]
+  
   def index
-    @product = Product.find(params[:item_id])
     @user_buy = UserBuy.new
   end
 
   def create
-    @product = Product.find(params[:item_id])
     @user_buy = UserBuy.new(buy_params)
     if @user_buy.valid?
-      Payjp.api_key = "sk_test_81e1c227014f57dc5d846694"
-      Payjp::Charge.create(
-        amount: buy_params[:price], 
-        card: buy_params[:token], 
-        currency: 'jpy'     
-      )
-      @user_buy.save
-      redirect_to root_path
+       pay_item
+       @user_buy.save
+       redirect_to root_path
     else
-      @product = Product.find(params[:item_id])
-      render action: :index
+       set_product
+       render action: :index
     end
   end
 
@@ -28,5 +23,19 @@ class OrdersController < ApplicationController
    def buy_params
     params.require(:user_buy).permit(:shipping_postal_code, :delivery_area_id, :shipping_municipality, :shipping_address, :shipping_building_name, :phone_number).merge(user_id:current_user.id, item_id: params[:item_id],token: params[:token],price:@product.price)
    end
+
+   def set_product
+    @product = Product.find(params[:item_id])
+   end
+
+   def pay_item
+    Payjp.api_key = "sk_test_81e1c227014f57dc5d846694"
+    Payjp::Charge.create(
+      amount: buy_params[:price], 
+      card: buy_params[:token], 
+      currency: 'jpy'     
+    )
+   end
+
 
 end
