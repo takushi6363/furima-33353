bd89e735de78738e1d02da6cac9434b34eb86d1f
takushi_akayama <akayama63@gmail.com>
2021-04-07 09:49:52 +0900

    ordersコントーラー修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,9 +6,15 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @product = Product.find(params[:item_id])
     @user_buy = UserBuy.new(buy_params)
-    binding.pry
     if @user_buy.valid?
+      Payjp.api_key = "sk_test_81e1c227014f57dc5d846694"
+      Payjp::Charge.create(
+        amount: buy_params[:price], 
+        card: buy_params[:token], 
+        currency: 'jpy'     
+      )
       @user_buy.save
       redirect_to root_path
     else
@@ -20,7 +26,7 @@ class OrdersController < ApplicationController
 
     private
    def buy_params
-    params.require(:user_buy).permit(:shipping_postal_code, :delivery_area_id, :shipping_municipality, :shipping_address, :shipping_building_name, :phone_number).merge(token: params[:token])
+    params.require(:user_buy).permit(:shipping_postal_code, :delivery_area_id, :shipping_municipality, :shipping_address, :shipping_building_name, :phone_number).merge(user_id:current_user.id, item_id: params[:item_id],token: params[:token],price:@product.price)
    end
 
 end
